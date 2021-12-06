3c3f40bd757ae423538c1e0c486c80cb41067427
Burak Kemal Koyuncu <burakkemalkoyuncu@gmail.com>
2021-12-06 11:06:00 +0300

    güzel bir başlangıç için ilk adım

---
+++ ReCapProjectApp/src/test/java/com/etiya/ReCapProjectApp/discovery/ReCapProjectApp/ReCapProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiya.ReCapProjectApp.discovery.ReCapProjectApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCapProjectAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
